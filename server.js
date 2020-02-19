e5f7991197d39007ec68565a744196b6f3576ba2
Dylan Di Vito <divitodylan@gmail.com>
2020-02-19 13:04:43 +0100

    console.log test

--- server.js
+++ server.js
@@ -139,12 +139,14 @@ const server = new ApolloServer({
 server.applyMiddleware({ app, path: '/graphql', cors: false}) 
 
 if(process.env.NODE_ENV === "production") {
+  console.log('ask client prod')
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }else{
+  console.log('ask client')
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
